f897565bc194bef693b02353b1cce2db13b7dd46
Imesh Gunaratne <imesh@apache.org>
2014-03-07 09:35:01 -0500

    Added new page

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- </body>
-</html>
+<head>
+  <title>PHP Demo</title>
+  <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+  <div id="main">
+  <h1>Welcome to Apache Stratos Alpha Demonstration</h1>
+  <h2>Web Site Main Ingredients</h2>
+  <p>A Home Page (index.php)</p>
+  <p>A Style Sheet (style.css)</p>
+  <p>Include files (footer.php)</p>
+
+<?php include("footer.php"); ?>
+</div>
+
+</body>
+</html> 
